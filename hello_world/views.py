ffa834d4d763bf761900a92d2c8bf9567375d7cd
Abhishek Verma <abhishekaverma20@gmail.com>
2024-06-08 07:19:20 +0530

    views update

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+
+def fibbonacci(n):
+    sequence = [0,1]
+    while len(sequence)<n:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
 
 def index(request):
-    return HttpResponse("Hello, world! from Abhishek Verma")
+    curr_time = datetime.datetime.now()
+    # return HttpResponse(f"Hello, world! from Abhishek Verma\nThe time is: {curr_time.strftime('%Y-%m-%d %H:%M:%S')}")
+    return HttpResponse(map(str,fibbonacci(10)))
